83866e54ee6ca98a31de9fc0ac997f74139fe47a
Abdullah M. Bakry <abdaullah62@gmail.com>
2024-10-26 15:43:23 +0300

    include Kinde auth provider

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -2,19 +2,22 @@
 
 import { Toaster } from "@/components/ui/sonner";
 import { persistor, store } from "@/lib/store/store";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-          <Toaster />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
+    <KindeProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </KindeProvider>
   );
 }
